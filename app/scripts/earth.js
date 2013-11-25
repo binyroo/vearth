8f3a4994e0f6334e1dd9262230c45b65b9aa2ede
binyroo <binyroo@gmail.com>
2013-11-25 15:29:06 +0900

    간단한 스피어 그리기

---
+++ app/scripts/earth.js
@@ -0,0 +1,74 @@
+
+var radius = 6371;
+var tilt = 0.41;
+
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
+
+var container;
+var camera, scene, renderer, stats;
+var geometry, meshPlanet;
+
+init();
+animate();
+
+function init() {
+
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(25, SCREEN_WIDTH / SCREEN_HEIGHT, 50, 1e7);
+    camera.position.z = radius * 8;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.SphereGeometry(radius, 100, 50);
+
+    meshPlanet = new THREE.Mesh(
+        geometry,
+        new THREE.MeshBasicMaterial({ color: 0xff3333 })
+    );
+
+    scene.add(meshPlanet);
+
+    renderer = new THREE.WebGLRenderer({
+        alpha: false
+    });
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    renderer.sortObjects = false;
+    renderer.autoClear = false;
+    container.appendChild(renderer.domElement);
+
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px';
+    stats.domElement.zIndex = 10000;
+    container.appendChild(stats.domElement);
+
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize(e) {
+
+    SCREEN_WIDTH = window.innerWidth;
+    SCREEN_HEIGHT = window.innerHeight;
+
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+    camera.updateProjectionMatrix();
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    render();
+    stats.update();
+}
+
+function render() {
+    renderer.clear();
+    renderer.render(scene, camera);
+}
